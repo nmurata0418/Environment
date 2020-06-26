56052a8d4cd6c1ce3628b8d99119efed27d8ae3a
nmurata0418 <nmurata@skywill.jp>
2020-06-26 10:35:02 +0900

    Environment 更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
 const path = require('path');
-
 const MODE = process.env.NODE_ENV;
 
-
-
 module.exports = {
-
     mode: MODE,
-
     entry: './src/js/index.js',
 
-
-
     output: {
-
     path: path.join(__dirname, 'dist'),
-
     filename: 'js/[name].js',
-
     },
 
-
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        watchContentBase: true,
+        port: 3000,
+        inline: true,
+        hot: true,
+    },
 
     resolve: {
-
         modules: ['node_modules'],
-
         extensions: ['.js'],
-
     },
-
 };
